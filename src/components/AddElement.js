0689ecc1345804d8ee4c856ed0fda496e7936d6b
fatma14 <fatmafadhli1@gmail.com>
2020-04-01 13:34:18 +0200

    add buttons to show elements

--- src/components/AddElement.js
+++ src/components/AddElement.js
@@ -1,22 +1,77 @@
 import React, { Component } from "react";
 
+// import "./addElement.css";
+const ACTIVE_STATE = "ACTIVE";
+const COMPLETED_STATE = "COMPLETED";
+const ALL_STATE = "ALL";
+
 export default class AddElements extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toDoPhrase: ""
+      toDo: {
+        toDoPhrase: "",
+        isDone: false
+      },
+      toDoArray: [],
+      filterMode: "ALL"
     };
   }
 
   handleChange(event) {
-    console.log(this);
     this.setState({
-      toDoPhrase: event.target.value
+      toDo: {
+        toDoPhrase: event.target.value,
+        isDone: false
+      }
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      toDoArray: [...this.state.toDoArray, this.state.toDo],
+      toDo: {
+        toDoPhrase: "",
+        isDone: false
+      }
     });
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  handleCheckBoxChange(index) {
+    console.log("handleCheckBoxChange", index);
+    const toDo = this.state.toDoArray.slice();
+    toDo[index].isDone = !toDo[index].isDone;
+    this.setState({
+      toDoArray: toDo
+    });
+  }
+
+  showActiveElements() {
+    this.setState({
+      filterMode: ACTIVE_STATE
+    });
+  }
+
+  showCompletedElements() {
+    this.setState({
+      filterMode: COMPLETED_STATE
+    });
+  }
+
+  showAllElements() {
+    this.setState({
+      filterMode: ALL_STATE
+    });
+  }
+
+  removeCompletedTasks() {
+    const activeTasksArray = this.state.toDoArray
+      .slice()
+      .filter(toDo => toDo.isDone === false);
+    console.log("activeTasksArray", activeTasksArray);
+    this.setState({
+      toDoArray: activeTasksArray
+    });
   }
 
   render() {
@@ -31,14 +86,80 @@ export default class AddElements extends Component {
             What needs to be done?
             <input
               type="text"
-              value={this.state.toDoPhrase}
+              value={this.state.toDo.toDoPhrase}
               onChange={event => {
                 this.handleChange(event);
               }}
+              placeholder="Add your todo here"
             />
           </label>
           <input type="submit" value="Submit" />
         </form>
+        <form>
+          {this.state.toDoArray
+            .filter(toDo => {
+              if (this.state.filterMode === ACTIVE_STATE) {
+                return toDo.isDone === false;
+              } else if (this.state.filterMode === COMPLETED_STATE) {
+                return toDo.isDone === true;
+              } else {
+                return true;
+              }
+            })
+            .map((toDo, index) => (
+              <div className="task-todo">
+                <label
+                  style={{
+                    textDecoration: toDo.isDone && "line-through"
+                  }}
+                  key={index}
+                >
+                  {toDo.toDoPhrase}
+                  <input
+                    name="isDone"
+                    type="checkbox"
+                    checked={toDo.isDone}
+                    onChange={event => {
+                      console.log(event);
+                      this.handleCheckBoxChange(index);
+                    }}
+                  />
+                </label>
+              </div>
+            ))}
+        </form>
+        <div>
+          {this.state.toDoArray.filter(toDo => toDo.isDone === false).length}
+          items left
+        </div>
+        <button
+          onClick={() => {
+            this.showActiveElements();
+          }}
+        >
+          Active
+        </button>
+        <button
+          onClick={() => {
+            this.showCompletedElements();
+          }}
+        >
+          Completed
+        </button>
+        <button
+          onClick={() => {
+            this.showAllElements();
+          }}
+        >
+          All
+        </button>
+        <button
+          onClick={() => {
+            this.removeCompletedTasks();
+          }}
+        >
+          Clear completed tasks
+        </button>
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// export default function Todo(props) {
+//   return (
+//     <div>
+//       <h3>{props.todo}</h3>
+//     </div>
+//   );
+// }
